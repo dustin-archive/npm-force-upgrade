724700c020b4b258922ca9180359c1901a2de236
Dustin Dowell <dustindowell22@gmail.com>
2018-02-27 15:10:18 -0600

    remove whitespace

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const x = y => Object.keys(y).map(z => `${z}@latest`).join(' ')
 
 readFile(resolve('package.json'), (err, data) => {
   if (err) throw err
-
   const { dependencies, devDependencies } = JSON.parse(data)
-
   exec(`npm i ${x(dependencies)}; npm i -D ${x(devDependencies)}`, { maxBuffer: Infinity }).stdout.pipe(process.stdout)
 })
