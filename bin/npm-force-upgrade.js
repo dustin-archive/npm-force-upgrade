cdcc5641a0eae4521ce6d3bc5058915abe3c3a9b
Brandon Semilla <semibran@gmail.com>
2018-03-02 15:43:24 -0800

    readability tweaks

--- bin/npm-force-upgrade.js
+++ bin/npm-force-upgrade.js
@@ -2,16 +2,18 @@
 var readFileSync = require('fs').readFileSync
 var execSync = require('child_process').execSync
 var resolve = require('path').resolve
-var pkg = JSON.parse(readFileSync(resolve('package.json')))
+var path = resolve('package.json')
+var pkg = JSON.parse(readFileSync(path))
 var deps = expand(pkg.dependencies)
 var devDeps = expand(pkg.devDependencies)
+var opts = { maxBuffer: Infinity, stdio: 'inherit' }
 
 if (deps) {
-  execSync('npm i ' + deps, { maxBuffer: Infinity, stdio: 'inherit' })
+  execSync('npm i ' + deps, opts)
 }
 
 if (devDeps) {
-  execSync('npm i -D ' + devDeps, { maxBuffer: Infinity, stdio: 'inherit' })
+  execSync('npm i -D ' + devDeps, opts)
 }
 
 function expand(deps) {
